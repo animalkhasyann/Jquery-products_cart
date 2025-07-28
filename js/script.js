a47ce8f7e5bb63fb010d026101ce457620782dad
animalkhasyann <amalkhasyan07@gmail.com>
2025-07-28 18:55:56 +0400

    products and cart page project

---
+++ js/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  updateCartCount();
+
+  $('.add-to-cart').click(function () {
+    const $btn = $(this);
+    const $product = $btn.closest('.product');
+    const id = $product.data('id');
+    const image=$product.data('image');
+    const name = $product.data('name');
+    const price = parseFloat($product.data('price'));
+    const qty = parseInt($product.find('.qty').val());
+
+    const product = { id, image, name, price, qty };
+
+    let cart = JSON.parse(sessionStorage.getItem('cart') || '[]');
+    const index = cart.findIndex(item => item.id === id);
+
+    if (index !== -1) {
+      cart[index].qty += qty;
+    } else {
+      cart.push(product);
+    } 
+
+
+    sessionStorage.setItem('cart', JSON.stringify(cart));
+    document.cookie = `cart=${encodeURIComponent(JSON.stringify(cart))}; path=/; max-age=86400`;
+
+    console.log("Cookie set:", document.cookie);
+
+    updateCartCount();
+
+    $btn.text('ADDED').prop('disabled', true);
+    setTimeout(() => {
+      $btn.text('ADD TO CART').prop('disabled', false);
+    }, 3000);
+  });
+
+  function updateCartCount() {
+    let cart = JSON.parse(sessionStorage.getItem('cart') || '[]');
+    let totalQty = cart.reduce((sum, item) => sum + item.qty, 0);
+    $('.cart-badge').text(totalQty);
+  }
+});
